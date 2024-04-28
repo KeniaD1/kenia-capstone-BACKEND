dd2b4ed6d9b389798464d722134512b1f4a8357e
keniaDE <decoteaukenia@gmail.com>
2024-04-28 10:27:16 -0400

    update to data in seed file

--- db/seed.sql
+++ db/seed.sql
@@ -1,6 +1,7 @@
 \c messages_dev
 
-INSERT INTO messages (name , post_date, post_time, posted_message, class) VALUES ('kenia decoteau','2024-03-07','12:51:00', 'Hello Everyone','Pre-k'),('Teacher Amie','2024-04-24', '11:02:00', 'Hello I am in class today thanks for being patient with the subsitute ','Pre-k'),('Tasha Pollock', '2024-04-26', '11:10:00', 'Please let me know all May birthdays ','Kindergarten'); 
+INSERT INTO messages (name , post_date, post_time, posted_message, class) VALUES ('Telonna Fabien','2024-03-07','12:51:00', 'Hello Everyone','Pre-k'),('Teacher Amie','2024-04-24', '11:02:00', 'Hello I am in class today thanks for being patient with the subsitute teacher 😃','Pre-k'),('Tasha Pollock', '2024-04-26', '11:10:00', 'Please let me know all May birthdays 🎂','Kindergarten'), ('Teacher Amie', '2024-04-28', '10:11:00','Hello Pre-k parents/guardians, i would like to express how much i appreciate you guys using this app to stay update on the classroom','Pre-k'); 
 
 
-INSERT INTO comments (message_id, user_name, comment_text, timestamp) VALUES (1, 'Teacher Amie','Hope all is well','2024-04-25 12:05:00'),(1, 'Kenia Decoteau', 'Yes thank you for asking', '2024-04-26 11:15:00');
+INSERT INTO comments (message_id, user_name, comment_text, timestamp) VALUES (1, 'Teacher Amie','Hope all is well','2024-04-25 12:05:00'),(1, 'Kenia Decoteau', 'Yes thank you for asking', '2024-04-26 11:15:00'),(2,'Telonna Fabien', 'Thank you for the heads up 🤗', '2024-04-28 10:09:00')
+;
